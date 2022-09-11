3b6d6cdd793767ccdb9c9ae7d9615441921162ca
sanju214 <85310807+sanju214@users.noreply.github.com>
2022-09-11 06:50:43 +0530

    added

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import {useState} from 'react'
+
+export default function Home() {
+  const [search,setSearch] = useState()
+
+
+  // js
+  return (
+<>
+
+<div class="bg-[url('/st-table.jpg')] min-h-full w-full h-screen bg-cover">
+<nav class="w-32 z-3 bg-indigo-500 h-full opacity-20 pl-5" id="mySidebar"><br/>
+
+  <div class="bg-indigo-500">
+    <h1 class="text-xl pb-10"><b>Chapters</b></h1>
+  </div>
+  <div class=" flex flex-col space-between-4">
+    <a href="chapter3" onclick="w3_close()" class="pb-2">Chapter 1</a>
+    <a href="#showcase" onclick="w3_close()" class="pb-2">Chapter 2</a>
+    <a href="#services" onclick="w3_close()" class="pb-2">Chapter 3</a>
+    <a href="#designers" onclick="w3_close()" class="pb-2">Chapter 4</a>
+    <a href="#packages" onclick="w3_close()" class="pb-2">Chapter 5</a>
+    <a href="#contact" onclick="w3_close()" class="pb-2">Chapter 6</a>
+  </div>
+</nav>
+</div>
+</>
+  )
+}
